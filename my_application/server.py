5ea860e75a83203fa1d578f5af1ea10780eea2ac
Ubuntu <ubuntu@ip-172-30-0-251.eu-west-1.compute.internal>
2015-10-11 09:37:56 +0000

    more endpoints on flask server

--- my_application/server.py
+++ my_application/server.py
@@ -1,9 +1,30 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
+def index():
+    return "Index File"
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
